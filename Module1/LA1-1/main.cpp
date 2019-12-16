9d890cfb7f39c80221c9e91d7ff13a67af7bc465
Alex Peterson <alexpeterson1@ad.weber.edu>
2019-12-16 09:53:22 -0700

    Added cout  and cin

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
+    int age; // int: signed integers
+    // cout: fpr system output
     std::cout << "Hello World" << std::endl;
+    cout << "Another line under namespace std" << endl;
+    cout << "Wildcats" << " WSU " << "Ogden" << endl;
+    // cin: for system/user input
+
+    cout << "What is your age?" << endl;
+    cin >> age;
+    cout << "I see you are: " << age << endl;
     return 0;
 }
