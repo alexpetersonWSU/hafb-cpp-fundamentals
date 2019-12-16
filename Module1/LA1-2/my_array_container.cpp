ae55d7530c941a3d3accab12d61ab212eff770ab
Alex Peterson <alexpeterson1@mail.weber.edu>
2019-12-16 15:38:43 -0700

    add for each loop

---
+++ Module1/LA1-2/my_array_container.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<array>     //array container
+using namespace std;
+
+int main()
+{
+    // To create arrays with library:
+    // array<type, numOfElements> name;
+    array<double, 5> grades{22.1, 6.9, 11.2, 99, 44.3};
+
+    cout <<"Size of array : " << grades.size() << endl;
+    for(int index = 0; index < grades.size(); ++index)
+    {
+        cout << grades[index] << endl;
+    }
+
+    // for each loop
+    cout << "For each: " << endl;
+    for(auto grade : grades)
+    {
+        cout << grade << endl;
+    }
+    return 0;
+}
