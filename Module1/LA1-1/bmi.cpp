6d0d76f2e20ab22e5226803489ec7bba2f97b5ef
Alex Peterson <alexpeterson1@mail.weber.edu>
2019-12-16 11:16:38 -0700

    bmi

--- Module1/LA1-1/bmi.cpp
+++ Module1/LA1-1/bmi.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 using namespace std;
 
+const float kMeterTOInches = 39.37;
+const float kKiloToPounds = 2.204;
+const float kBMIImperial = 703;
+
 int main()
 {
     //calculate BMI: weight(kg)/[height(m)]^2
     float weight = 0, height = 0, bmi = 0;
 
-    cout << "Please enter your weight: ";
+    cout << "Please enter your weight in kg: ";
     cin >> weight;
 
-    cout << "Please enter your height: ";
+    cout << "Please enter your height in meters: ";
     cin >> height;
 
     bmi = weight / (height * height);
-    cout << "Your BMI is " << bmi;
+    cout << "Your BMI is " << bmi << endl;
+
+    //calcualte again in imperial
+
+    weight = weight * kKiloToPounds;
+    height = height * kMeterTOInches;
+
+    bmi = weight * kBMIImperial / (height * height);
+    cout << "Your imperial BMI is " << bmi << endl;
+
     return 0;
 }
