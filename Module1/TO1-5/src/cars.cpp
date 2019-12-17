3f82fd223eaac80d9ad5a5a0cbc8ec0c9155af5e
Alex Peterson <alexpeterson1@mail.weber.edu>
2019-12-17 14:06:49 -0700

    namespace

--- Module1/TO1-5/src/cars.cpp
+++ Module1/TO1-5/src/cars.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
 #include "cars.h"
+
+int HorsePower() // "nameless" namespace
+{
+    return 150;
+}
 // using namespace lamborghini;
+int lamborghini::HorsePower() // lamborghini namespace
+{
+    return 760;
+}
 // using namespace porsche;
+int porsche::HorsePower() // porsche namespace
+{
+    return 700;
+}
+
+void lamborghini::cout()
+{
+    std::cout << "Lamborghini" << std::endl;
+}
