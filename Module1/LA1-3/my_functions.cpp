e15f949fdd02c6228c6d90b756f34c5e0d732a18
Alex Peterson <alexpeterson1@mail.weber.edu>
2019-12-17 11:20:17 -0700

    added pass by referance

--- Module1/LA1-3/my_functions.cpp
+++ Module1/LA1-3/my_functions.cpp
@@ -1,3 +1,4 @@
+#include "my_functions.h"
 // Function definition
 /**
  * @brief Sum num1 & num2
@@ -6,7 +7,19 @@
  * @param num2 
  * @return int 
  */
-int Sum(int num1, int num2)
+int Sum(int num1, int num2) // pass both parameters by value (make your own copy)
 {
   return num1 + num2;
+}
+
+/**
+ * @brief Increments the value of step by 1
+ * 
+ * @param step 
+ */
+void UpdateStep(int& step) // take the address of the parameter
+{
+    cout << "Current step: " << step << endl;
+    step++;
+    cout << "New step: " << step << endl;
 }
--- Module1/LA1-3/my_functions.h
+++ Module1/LA1-3/my_functions.h
@@ -5,3 +5,5 @@ using namespace std;
 
 // prototype
 int Sum(int num1, int num2);
+
+void UpdateStep(int& step);
