ed997f864df8a36d222252dfcacf32fdb7d6cca6
Alex Peterson <alexpeterson1@mail.weber.edu>
2019-12-19 14:38:47 -0700

    use vector constructor

--- Module5/LA5-1/my_array.cpp
+++ Module5/LA5-1/my_array.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-using namespace std;
 #include <array>
-#include <algorithm>
+#include <algorithm> //for iterators
 
 // Local Prototypes
 void fun(int const& value); 
@@ -9,6 +8,46 @@ void fun(int const& value);
 
 int main()
 {
+    std::array<int, 3> info = {4, 7, 9};
+    std::cout<<"First element: " << info.front() << std::endl;
+    std::cout<<"Last element: " << info.back() << std::endl;
+    std::cout<<"Size of Array: " << info.size() << std::endl;
+    //iterate over elements
+    for(size_t i = 0; i < info.size(); i++)
+    {
+        std::cout << &info[i] << " " << info[i] << std::endl;
+    }
+    std::cout << "\n";
+    std::cout << "Loop using iterator notation. Using built-in functions for"
+        << " begin and end members of containers" << std::endl;
+    for(auto it = std::begin(info); it != std::end(info); ++it)
+    {
+        //std::cout << it <<std::endl;    // it is a pointer (address)
+        std::cout << it << " " << *it <<std::endl;   // *it: the value at the address
+    }
+
+    std::cout << std::endl;
+    //Range based loops
+    std::cout<<"Range based for loop" << std::endl;
+    for(const auto& value : info)
+    {
+        std::cout << value << std::endl;
+    }
+    std::cout << std::endl;
+
+    //For each loops
+    // functor: function that acts like an object
+    std::cout << "Using for_each algorithm, using a function or functor" << std::endl;
+    std::for_each(std::begin(info), std::end(info), fun);
+    std::cout << std::endl;
+
+    //Lambdas: [](){}
+    // [] to take any local variables in the scope
+    // () declare the parameters of your function
+    // {} declare the body of your function
+    std::cout << "Using for_each algorithm, using lambdas" << std::endl;
+    std::for_each(std::begin(info), std::end(info), [](int const& value){std::cout<<value<<"\n";});
+
     return 0;
 }
 
--- Module5/LA5-1/my_vectors.cpp
+++ Module5/LA5-1/my_vectors.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
 
 int main()
 {
+    std::vector<int> vec;
+    // Add elements
+    vec.push_back(100); // now both, front() and back() contain 100
+    vec.push_back(200); // front 100 back 200
+    std::cout<< "Front of vector: " << vec.front() << std::endl;
+    std::cout<< "Back of vector: " << vec.back() << std::endl;
+
+    vec.push_back(300);
+    // Iterate over the vector
+    std::cout << "Using index notation" << std::endl;
+    for (size_t i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i] << std::endl;
+    }
+
+    std::cout << "Using an iterator" << std::endl;
+    for(auto it = std::begin(vec); it != std::end(vec); ++it)
+    {
+        std::cout << *it << std::endl; // de-reference the value "it" points to
+    }
+
+    std::cout << "Range based for loop" << std::endl;
+    for(const auto& value : vec)
+    {
+        std::cout << value << std::endl;
+    }
+
+    //Define and initialize a vector. Use {}  with comma seperated values
+    //std::vector<int> vec2 = {-1, -1, -1, -1, -1};
+    int max = 5;
+    std::vector<int> vec2(max, -1); // use vector constructor
+    for(int x : vec2)
+    {
+        std::cout<< x << " ";
+    }
+    std::cout<<std::endl;
 
     return 0;
 }
